d14dbafb34cfcb3b066bd9261d4948afec3fb1f0
Phat <ptnguyen728@gmail.com>
2023-06-20 22:16:10 -0500

    completed

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-
 from .models import Attendee
 
 
@@ -23,7 +22,14 @@ def api_list_attendees(request, conference_id):
         ]
     }
     """
-    return JsonResponse({})
+    attendees = [
+        {
+            "name": a.name,
+            "href": a.get_api_url(),
+        }
+        for a in Attendee.objects.filter(conference=conference_id)
+    ]
+    return JsonResponse({"attendees": attendees})
 
 
 def api_show_attendee(request, id):
@@ -46,4 +52,20 @@ def api_show_attendee(request, id):
         }
     }
     """
-    return JsonResponse({})
+
+    attendee = Attendee.objects.get(id=id)
+    return JsonResponse(
+        {
+            "email": attendee.email,
+            "name": attendee.name,
+            "company_name": attendee.company_name,
+            "created": attendee.created,
+            "conference": {
+                "name": attendee.conference.name,
+                "href": attendee.conference.get_api_url(),
+            }
+        }
+    )
+    # attendee = list(Attendee.objects.values())
+
+    # return JsonResponse({"attendee": attendee})
